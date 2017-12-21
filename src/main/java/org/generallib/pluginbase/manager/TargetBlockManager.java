73f956cf9b10c203876b6ac17760a1df496a7edf
wysohn <wysohn2002@naver.com>
2017-12-20 20:21:59 -0800

    This should be canceled

--- src/main/java/org/generallib/pluginbase/manager/TargetBlockManager.java
+++ src/main/java/org/generallib/pluginbase/manager/TargetBlockManager.java
@@ -56,6 +56,8 @@ public class TargetBlockManager extends PluginManager<PluginBase> implements Lis
         if(!callbackMap.containsKey(uuid))
             return;
 
+        e.setCancelled(true);
+
         if(e.getPlayer().isSneaking()) {
             callbackMap.remove(uuid);
         }else {
